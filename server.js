e2204a8e0e7bd79b9757044e797cc7eeca26d144
christiankasegne <christiankasegne@gmail.com>
2023-05-17 22:08:39 -0400

    restructured file and updatet server.js file

---
+++ server.js
@@ -0,0 +1,48 @@
+const fs = require(`fs`);
+const express = require(`express`);
+const path = require(`path`);
+const notes = require(`./Develop/db/db.json`);
+const uuid = require('uuid');
+const { isUtf8 } = require('buffer');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(`public`));
+
+app.get(`/notes`, (req, res) => {
+    res.sendFile(path.join(__dirname, `/db/db.jso`))
+});
+
+//add note to db file
+app.post(`/api/notes`, (req, res) => {
+    const notes =JSON.parse(fs.readFileSync(`./db/db.json`));
+    const newNotes= req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileAsync(`/db/db.json`, JSON.stringify(notes));
+    res.json(notes);
+});
+//delete Notes from db file
+app.delete(`/api/notes/:id`, (req, res) => {
+    const notes = JSON.parse(fs.readFileSync(`./db/db.json`));
+    const deleteNote = notes.filter((removeNote) => removeNote.id !== req.params.id);
+    fs.writeFileSync(`./db/db.json`, JSON.stringify(deleteNote));
+    res.json(deleteNote);
+})
+
+
+app.get(`/`, (req, res) => {
+    res.sendFile(path.join(__dirname, `public/index.html`))
+  });
+app.get(`/notes`, (req, res) => {
+  res.sendFile(path.join(__dirname, `public/notes.html`))
+});
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening on PORT` + PORT)
+);
